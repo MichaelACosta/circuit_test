cdc6ecd07529980fd7231a47c5415f0bb087c138
Michael Costa <macosta@inf.ufpel.edu.br>
2018-12-13 00:16:57 -0200

    changed the goAhead without ROS
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- CircuitTest.py
+++ CircuitTest.py
@@ -7,19 +7,22 @@ def turnLeft():
 def turnRight():
     print('Turn Right')
 
+def stopGoAhead(odometry, meters):
+    return (odometry/1200.0)*3.14*0.34 >= meters
+
 def goAhead(meters):
-    print(meters)
+    print(stopGoAhead(4320.0, meters))
 
 def listener():
-    goAhead(9)
+    goAhead(9.0)
     turnLeft()
-    goAhead(5)
+    goAhead(5.0)
     turnRight()
     goAhead(7.8)
     turnLeft()
     goAhead(3.6)
     turnLeft()
-    goAhead(3)
+    goAhead(3.0)
     turnLeft()
     goAhead(3.6)
     turnRight()
