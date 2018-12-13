87480f0687f72b4205163c929bc04301a2cd8978
Michael Costa <macosta@inf.ufpel.edu.br>
2018-12-12 23:37:33 -0200

    added route in the structure of code
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- CircuitTest.py
+++ CircuitTest.py
@@ -1,14 +1,32 @@
+def stop():
+    print('Stop')
+
 def turnLeft():
-    None
+    print('Turn Left')
 
 def turnRight():
-    None
+    print('Turn Right')
 
-def goAhead():
-    None
+def goAhead(meters):
+    print(meters)
 
 def listener():
-    None
+    goAhead(9)
+    turnLeft()
+    goAhead(5)
+    turnRight()
+    goAhead(7.8)
+    turnLeft()
+    goAhead(3.6)
+    turnLeft()
+    goAhead(3)
+    turnLeft()
+    goAhead(3.6)
+    turnRight()
+    goAhead(13.2)
+    turnLeft()
+    goAhead(5.4)
+    stop()
 
 if __name__ == '__main__':
     listener()
